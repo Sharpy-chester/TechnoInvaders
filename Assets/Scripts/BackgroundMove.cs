d29073fca8d0c100d45d7e435f04750dced3ec4e
Sharpy <cam-chester@hotmail.co.uk>
2018-08-31 01:13:14 +0100

    Add moving background

---
+++ Assets/Scripts/BackgroundMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMove : MonoBehaviour
+{
+
+    public float speed = 0.1f;
+
+    void Update()
+    {
+        Material mat = this.GetComponent<MeshRenderer>().material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.y -= Time.deltaTime * speed;
+        mat.mainTextureOffset = offset;
+    }
+}
